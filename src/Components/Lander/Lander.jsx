1c859456d6d3e354ac030a6ce9254c9864b0b1d4
SabinGhimireNP <sabinghimire223@gmail.com>
2026-01-06 13:10:48 +0545

    About me

--- src/Components/Lander/Lander.jsx
+++ src/Components/Lander/Lander.jsx
@@ -32,6 +32,32 @@ function Lander() {
 
     {/* About me */}
     <div className="AboutMe">
+      <span>About Me</span>
+      <span> <h1> What I do</h1></span>
+      <span>Namaste! I'm Sanchit Pandey a skilled Front-End Developer, transforming designs into captivating web experiences. Proficient in HTML, CSS, JavaScript, React and Tailwind, I merge aesthetics with functionality to craft user-centric sites. Let's create a digital masterpiece together!</span>
+      <div>
+        <div className="btn">
+          <img src="" alt="" />
+          <div>
+            <h2>Graphics Design</h2>
+            <p>रंगिन अनुभव प्रदान गर्दै</p>
+          </div>
+        </div>
+        <div className="btn">
+          <img src="" alt="" />
+          <div>
+            <h2>UI/UX Design</h2>
+            <p>रंग र रूपले नयाँ कथा बोलाउँदै</p>
+          </div>
+        </div>
+        <div className="btn">
+          <img src="" alt="" />
+          <div>
+            <h2>Web Development</h2>
+            <p>वेब विकासमा भविष्य निर्माण गर्दै |</p>
+          </div>
+        </div>
+      </div>
     </div>
 
     {/* Skills */}
